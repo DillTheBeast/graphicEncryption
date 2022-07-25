f0b383b209f60e1c1df52cf924eeafec7f8b2c5e
DillTheBeast <dillonmaltese@gmail.com>
2022-07-25 13:46:45 -0400

    j

--- src/Encryption/Main.java
+++ src/Encryption/Main.java
@@ -12,7 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Encryption.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
